d93329e1699dd60febe81215d6a36ede7100d9b1
dsoma <deepak.somashekhara@gmail.com>
2025-07-20 22:38:17 -0700

    Add biome linting to the project

---
+++ .github/pull_request_template.md
@@ -0,0 +1,5 @@
+## Purpose
+
+## Changes
+
+## References
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import DharaPlayer from './dhara-player';
 
 const win: any = window;
+
+// biome-ignore lint/complexity/useLiteralKeys: Prevent name minification
 win['DHARA'] = win['DHARA'] || {};
+
+// biome-ignore lint/complexity/useLiteralKeys: Prevent name minification
 win['DHARA']['Player'] = DharaPlayer;
