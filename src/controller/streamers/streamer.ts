ede090b274a5892211820a3e1a7f477ee88f9f1f
dsoma <deepak.somashekhara@gmail.com>
2025-07-30 14:03:34 -0700

    Add process tick timer

--- src/controller/streamers/streamer.ts
+++ src/controller/streamers/streamer.ts
@@ -1,8 +1,22 @@
 import type Media from '../../model/media';
 import type AdaptationSet from '../../model/adaptation-set';
+import type Representation from '../../model/representation';
+import type Period from '../../model/period';
+import type SegmentBase from '../../model/segment-base';
 import type NativePlayer from '../native-player';
 import Buffer from './buffer';
 import log from 'loglevel';
+
+class StreamerState {
+    public curRep: Representation | null = null;
+    public curPeriod: Period | null = null;
+    public curSegment: SegmentBase | null = null;
+    public curPeriodIndex: number = 0;
+    public curRepIndex: number = 0;
+    public curSegmentIndex: number = 0;
+}
+
+const PROCESS_TICK = 20; // in milliseconds
 
 /**
  * Streamer is a class that handles the streaming of a media stream.
@@ -14,6 +28,9 @@ export default class Streamer {
     protected readonly _nativePlayer: NativePlayer;
     protected readonly _adaptationSet: AdaptationSet;
     protected _buffer: Buffer | null = null;
+    protected _state: StreamerState = new StreamerState();
+    protected _timer: NodeJS.Timeout | null = null;
+    protected _name: string = this.constructor.name;
 
     constructor(media: Media, adaptationSet: AdaptationSet, nativePlayer: NativePlayer) {
         this._media = media;
@@ -40,6 +57,9 @@ export default class Streamer {
             return false;
         }
 
+        this._state.curPeriod = this._media.periods?.[this._state.curPeriodIndex] ?? null;
+        this._state.curRep = this._adaptationSet.representations?.[this._state.curRepIndex] ?? null;
+
         return true;
     }
 
@@ -49,6 +69,29 @@ export default class Streamer {
     }
 
     public onPlay() {
-        log.info(`[Streamer] onPlay`);
+        log.info(`[${this._name}] onPlay`);
+        this.start();
+    }
+
+    public onPause() {
+        log.info(`[${this._name}] onPause`);
+        this.stop();
+    }
+
+    public start() {
+        this._timer = setInterval(() => {
+            this._process();
+        }, PROCESS_TICK);
+    }
+
+    public stop() {
+        if (this._timer) {
+            clearInterval(this._timer);
+            this._timer = null;
+        }
+    }
+
+    protected _process() {
+        //
     }
 }
